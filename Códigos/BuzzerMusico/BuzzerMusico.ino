ec32c5f1c1db95b152c15f550856f380fd13eb6a
RenanBGLaptopLinux <renanbezerraguima@proton.me>
2025-07-07 19:18:45 -0300

    vault backup: 2025-07-07 19:18:45

--- Códigos/BuzzerMusico/BuzzerMusico.ino
+++ Códigos/BuzzerMusico/BuzzerMusico.ino
@@ -9,9 +9,9 @@ void setup() {
 void loop() {
   tone(OUTPUT_PIN, random(1, 30)); // Frequências superiores muito altas são ruins
   //delay(10); // Intervalo constante e curto -> ruído
-  delay(200); // Intervalo constante BPM maior -> música
+  //delay(200); // Intervalo constante BPM maior -> música
   //delay(300);// Intervalo constante BPM menor-> música
-  //delay(random(300, 500)); // Intervalo incosistente e longo -> música
+  delay(random(300, 500)); // Intervalo incosistente e longo -> música
   //delay(random(1, 20)); // Intervalo inconsistente e curto -> ruído
   //delay(random(1, 500)); // Intervalo inconsistente -> ruído mais parecido com música
 }
